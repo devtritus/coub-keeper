c7ccd8262686e8157884540135fe6c79554d2250
devtritus <devtritus@gmail.com>
2022-06-30 02:44:07 +0300

    get and save requests from coub

--- main.go
+++ main.go
@@ -1,20 +1,72 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"strconv"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	"context"
 )
 
 func main() {
-	getPage()
-	fmt.Println("Keeper was closed")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	credential := options.Credential{
+		Username: "user",
+		Password: "pass",
+	}
+	opts := options.Client().ApplyURI("mongodb://localhost:27017").SetAuth(credential)
+	client, err := mongo.Connect(ctx, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			fmt.Println("Heello")
+			log.Fatal(err)
+		}
+	}()
+
+	fmt.Println("Heello1")
+	ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	fmt.Println("Heello2")
+	err = client.Ping(ctx, readpref.Primary())
+	fmt.Println("Heello3")
+	if err != nil {
+		fmt.Println("Heello4")
+		log.Fatal(err)
+	}
+
+	fmt.Println("Heello5")
+
+	collection := client.Database("vault").Collection("coubs")
+
+	page, totalPages, data := getPage(1)
+
+	for i := page; i < totalPages; i++ {
+		page, totalPages, data = getPage(i)
+		fmt.Println(i, page, totalPages)
+		res, err := collection.InsertOne(context.TODO(), data)
+		if err != nil {
+			log.Fatal("can't insert", err)
+		} else {
+			fmt.Println(res)
+		}
+	}
 }
 
-func getPage() {
+func getPage(nextPage int) (page, totalPages int, data map[string]interface{}) {
 	jar, err := cookiejar.New(nil)
 	if err != nil {
 		log.Fatal("Got error while creating cookie jar")
@@ -31,7 +83,7 @@ func getPage() {
 
 	cookie := &http.Cookie{
 		Name:  "remember_token",
-		Value: "7a17afe7de09e5aa0c209921745a9dedff5724f2",
+		Value: "7a17afe7de09e5aa0c209921745a9dedff5734f2",
 	}
 
 	urlObj, _ := url.Parse("https://coub.com/")
@@ -42,8 +94,11 @@ func getPage() {
 
 	q.Add("all", "true")
 	q.Add("order_by", "date")
-	q.Add("page", "1")
+	q.Add("page", strconv.Itoa(nextPage))
 
+	req.URL.RawQuery = q.Encode()
+
+	fmt.Println(req)
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println("Errored when sending request to the server")
@@ -51,13 +106,22 @@ func getPage() {
 	}
 
 	defer resp.Body.Close()
-	responseBody, err := ioutil.ReadAll(resp.Body)
+	content, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
+
+		return -1, -1, nil
 	} else {
-		fmt.Println(string(responseBody))
+		//fmt.Println(string(content))
+		return parseBody(content)
 	}
+}
 
-	fmt.Println(resp.Status)
+func parseBody(content []byte) (page, totalPages int, data map[string]interface{}) {
+	json.Unmarshal(content, &data)
 
+	page = int(data["page"].(float64))
+	totalPages = int(data["total_pages"].(float64))
+
+	return
 }
